e3a893491cb8ef004978622f5154bac2f104c9ed
EthanC64 <64redbrick@gmail.com>
2023-09-24 09:32:35 -0700

    server

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 // get the client
 const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "trashapp_db",
-    password: "rootroot",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD
 });
 
 connection.connect((err) => {
